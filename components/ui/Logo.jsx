7a4cfa9cd50412872284fb3259454ea8d3869e1f
karan <karan@gmail.com>
2024-01-05 01:21:00 +0530

    fix logo

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/">
-      <span className="text-2xl font-bubblegum font-bold cursor-pointer z-50 text-red-500">
+      <span className="mt-2 text-2xl font-bubblegum font-bold cursor-pointer z-50 text-red-500">
         KANGO CASTLE
       </span>
     </Link>
