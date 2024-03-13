1e2b46eef311fb4642cfcbee9301b0ce8cae904d
karan <karan@gmail.com>
2024-03-13 21:10:54 +0530

    adding git

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["res.cloudinary.com", "taran.futuretouch.org"],
+    domains: ["kangocastle.com"],
+    unoptimized: true
   },
 };
 
