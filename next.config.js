5e564cf41ae70b9dd9034ad2bb11c8ca9c34efe4
karan <karan@gmail.com>
2024-03-14 21:49:19 +0530

    fix again

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["kangocastle.com", "taran.futuretouch.org"],
+    domains: ["taran.futuretouch.org"],
     unoptimized: true
   },
 };
