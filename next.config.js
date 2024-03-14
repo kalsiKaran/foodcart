502b615790f0fa357982a3ec348f65b30a2f8128
karan <karan@gmail.com>
2024-03-14 21:09:37 +0530

    fix new api data

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["kangocastle.com"],
+    domains: ["kangocastle.com", "taran.futuretouch.org"],
     unoptimized: true
   },
 };
